6adfe5501431cdf5ebe082f139ea6d929c9209f7
Jeetendra Dhall <jeetendra.dhall@gmail.com>
2019-11-13 12:49:41 +0530

    Trie - get_child, form_strings_using_children, print, find_strings_with_prefix

--- Trie.py
+++ Trie.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 from IPython.core.debugger import set_trace
+from collections import deque
 
 
-# In[1]:
+# In[47]:
 
 
 class TrieNode:
@@ -45,6 +46,35 @@ class TrieNode:
 
         #continue nested insertion
         node.insert (word[1:])
+        
+    #get child corresponding to a char
+    def get_child (self, ch):
+        node = None
+        #iterate over children and look for a matching char
+        for child in self.children:
+            if child.char == ch:
+                node = child
+                break
+                
+        return node
+    
+    #form strings from current node to all leaf nodes
+    def form_strings_using_children (self):
+        #no more string formation at leaf node, form string using self.char
+        if len(self.children) == 0:
+            return [self.char]
+        
+        strings = []
+        l_substr = []
+        
+        for child in self.children:
+            l_substr.extend(child.form_strings_using_children ())
+        
+        #print ('substrings ', l_substr, 'for node', self.char)
+        for substr in l_substr:
+            strings.append (self.char + substr)
+        
+        return strings
     
     @classmethod
     def create_trie (cls, words):
@@ -65,16 +95,80 @@ class TrieNode:
 
             #insert the word in the trie value node
             node.insert (word[1:])
+    
+    @staticmethod
+    def print ():
+        #queue first level siblings
+        qu = deque (TrieNode.trie.values())
+        #insert marker after siblings
+        qu.append (TrieNode('.'))
+        while qu:
+            item = qu.popleft ()
+            print (item.char, end = " ")
+            #queue nested level siblings
+            qu.extend (item.children)
+            #insert marker after siblings
+            if item.char != '.':
+                qu.append (TrieNode('.'))
+
+    @classmethod
+    def find_strings_with_prefix (cls, prefix):
+        #prefix is required
+        if len(prefix) == 0:
+            return None
+        
+        #strings found with prefix
+        strings = []
+        #which (char at) index of prefix we are currently looking for
+        idx = 0
+        
+        #traverse down the trie using prefix for navigation
+
+        #get the trie header for the first character
+        key = prefix[idx]
+        if key in TrieNode.trie.keys ():
+            node = TrieNode.trie [key]
+        else:
+            #we do not have any strings with the given prefix
+            return []
+
+        #we now process further starting from the second char in the prefix
+        idx += 1
+
+        #traverse down the tree till the prefix is found
+        
+        #while we are still processing the prefix, and we haven't gone past the leaf
+        while idx < len (prefix) and node != None:
+            
+            #get to the child node based on the char in the prefix
+            node = node.get_child (prefix[idx])
+            
+            #get to the next char in the prefix for the next iteration
+            idx += 1
+            
+        #we should now either be at a node where the prefix ended (valid node)
+        #or the prefix was not present (node == None)
+        if node == None:
+            return []
+        
+        #get substrings (actually postfix strings)
+        l_substr = node.form_strings_using_children ()
+        
+        #combine prefix with all postfix strings
+        for substr in l_substr:
+            strings.append (prefix[:-1] + substr)
+            
+        return strings
 
 
-# In[2]:
+# In[48]:
 
 
 words = {'bear', 'stop', 'buy', 'sell', 'bid', 'stock', 'bell', 'bull'}
 TrieNode.create_trie (words)
 
 
-# In[6]:
+# In[49]:
 
 
 print(len(TrieNode.trie['b'].children))#b[u/i/e]
@@ -83,8 +177,20 @@ print(TrieNode.trie['b'].children[0].char)
 print(len(TrieNode.trie['b'].children[0].children))#bull, buy
 
 
-# In[ ]:
+# In[50]:
 
 
+TrieNode.print()
 
+
+# In[51]:
+
+
+TrieNode.trie['b'].form_strings_using_children()
+
+
+# In[63]:
+
+
+TrieNode.find_strings_with_prefix ('b')
 
