edbbfea9db8733cac2c4a01da467f9146db44e89
Jeetendra Dhall <jeetendra.dhall@gmail.com>
2019-12-02 14:38:47 +0530

    Optimizations - terminate if no min_weight updates, process only vertices whose cost changed in the previous iteration

--- Single-Source Shortest Path - General Weights (Dynamic Programming).py
+++ Single-Source Shortest Path - General Weights (Dynamic Programming).py
@@ -14,7 +14,7 @@ zero_based_vertices = False
 #Horowitz, Section 5.4 - SSSP-General Weights
 #define edges of the graph
 edges = {(1,2,6), (1,3,5), (1,4,5), (2,5,-1), (3,2,-2), 
-         (3,5,1), (4,3,-2), (4,6,-1), (5,7,3), (6,7,3)}
+         (3,5,1), (4,3,-2), (4,6,-1), (5,7,3), (6,7,2)}
 
 
 # In[3]:
@@ -26,7 +26,7 @@ if zero_based_vertices:
     n_vertices += 1
 
 
-# In[74]:
+# In[4]:
 
 
 #initialize the adjacency matrix
@@ -38,7 +38,7 @@ np.fill_diagonal (adj_matrix, 0)
 P = []
 
 
-# In[75]:
+# In[5]:
 
 
 #fill the matrix with edge weights
@@ -52,7 +52,7 @@ for edge in edges:
 print (adj_matrix)
 
 
-# In[115]:
+# In[6]:
 
 
 def get_path (u, v, P):
@@ -68,35 +68,70 @@ def get_path (u, v, P):
     return path, cost
 
 
-# In[91]:
+# In[9]:
 
 
+import queue
+from IPython.core.debugger import set_trace
 def bellman_ford (u):
+    #count number of steps
+    O_n_steps = 0
     #cost from source vertex
     cost = list(adj_matrix[u])
     #least cost incoming vertex
     path = np.ones ((n_vertices), dtype=int) * u
     #set of all non-source vertices
     S = list(filter(lambda x: x!=u, range(n_vertices)))
+    #changing vertices, previous and current iteration
+    v_changing_prev = S.copy ()
+    v_changing = []
+
     #iterate for max path length = (n_vertices - 1) times
     for k in range (n_vertices):
+        changing = False
         #for each iteration, revise shortest incoming edge
         for j in S:
-            #get shortest weight incoming vertex in column j
-            i, w = min(list(filter(lambda u: u[0]!=j and u[1]!=np.inf,
-                enumerate(adj_matrix[:,j]) )), key = lambda x: x[1])
-            cost_i_to_j = cost[i] + adj_matrix[i][j]
-            if cost_i_to_j < cost[j]:
-                print (i,j, cost[j], cost_i_to_j)
-                cost[j] = cost_i_to_j #update cost
-                path[j] = i #update least weigh incoming vertex
-    return cost, path
+
+            #was minimum updated for this vertex?
+            min_updated = False
+            #iterate over incoming edges
+            for i, w in list(filter( lambda u: 
+                                 u[0]!=j and u[1]!=np.inf and u[0] in v_changing_prev,
+                                 enumerate(adj_matrix[:,j]) )):
+                cost_i_to_j = cost[i] + adj_matrix[i][j]
+                if cost_i_to_j < cost[j]:
+                    print (i,j, cost[j], cost_i_to_j)
+                    cost[j] = cost_i_to_j #update cost
+                    path[j] = i #update least weigh incoming vertex
+                    changing = True #keep iterating k
+                    min_updated = True 
+
+                O_n_steps += 1
+            #queue of changing cost vertices
+            if min_updated:
+                v_changing.append(j)
+                
+        if not changing:
+            break
+        
+        #prepare queues for the next iteration
+        print (v_changing)
+        v_changing_prev = v_changing.copy()
+        v_changing = []
+
+    return cost, path, O_n_steps
 
 
-# In[117]:
+# In[10]:
 
 
-cost, path = bellman_ford (0)
-print(get_path (0, 4, path))
-print (cost)
+cost, path, O_n_steps = bellman_ford (0)
+print(get_path (0, 6, path))
+print (O_n_steps)#70->30->14
+
+
+# In[ ]:
+
+
+
 
